cc0001932ea36e1e9e29f2e4bef9fa25158ac3f2
joey2000cal <ajthurman@mines.edu>
2022-02-23 15:55:09 -0700

    Combine Code

---
+++ Miniproject/Arduino_Code/combinedcode/combinedcode.ino
@@ -0,0 +1,171 @@
+
+#include <Wire.h>
+
+
+#define SLAVE_ADDRESS 0x04
+int read_offset = 0;
+int state = 0;
+int len = 0;
+int in_data[32] = {};
+int desired_angle = -1;
+
+// A program that reads the direction of two Encoders and calculates movement. 
+// By Joey Thurman 2/23/2022
+
+#define r 0.05 
+#define b 0.1
+#define rot 3200
+#define CLK 2
+#define DT 4
+#define CLK2 3
+#define DT2 5 
+#define pi 3.14159
+
+volatile int ang_right = 0; 
+volatile int ang_left = 0;
+
+volatile int time_l = 0;
+volatile int time_r = 0; 
+
+volatile float theta_r;
+volatile float theta_l;
+
+volatile float x = 0;
+volatile float y = 0; 
+volatile float phi = 0; 
+
+volatile byte data = 0;
+
+
+void setup() {
+  //Enable serial communication
+    pinMode(13, OUTPUT);
+  Serial.begin(115200); //start serial for output
+  
+  //initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+
+  //define callabcks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+  Serial.println("Ready!");
+// initialize digital pin 
+
+  pinMode(CLK, INPUT_PULLUP);
+  pinMode(DT, INPUT_PULLUP);
+  pinMode(CLK2, INPUT_PULLUP);
+  pinMode(DT2, INPUT_PULLUP);
+
+ // Create an ISR attached to pin 2 on a rising edge. 
+  attachInterrupt(digitalPinToInterrupt(CLK), rotate_r, RISING);
+  attachInterrupt(digitalPinToInterrupt(CLK2), rotate_l, RISING);
+
+  
+  
+}
+
+void receiveData(int byteCount){
+  int i = 0;
+  if(byteCount > 1){
+    while(Wire.available()){
+      in_data[i] = Wire.read();
+      //Serial.print(in_data[i]);
+      //Serial.print(' ');
+      i++;
+    }
+    //Serial.print(' ');
+    i--;
+    len = i;
+  }
+  else{
+    read_offset = Wire.read();
+  }
+  desired_angle = in_data[1];
+ 
+  
+}
+
+void sendData(){
+  byte data[32] = {};
+  //if(read_offset == in_data[0]){
+    String val = String(ang_right);
+    for(int i = 0; i < val.length(); i++){
+      data[i] = val[i];
+  //}
+  Wire.write(data, 32);
+}
+}
+
+// the loop function runs over and over again forever
+void loop() {
+      
+      //Print the movement When their is new encoder data.
+      
+      theta_r = (float) ang_right*rot / (2*pi); 
+      theta_l = (float) ang_left*rot / (2*pi); 
+
+       
+      
+      
+      
+}
+
+//When a rising edge on pin 2 is detected check the direction of the Encoder based on previous inputs. 
+void  rotate_r(){
+
+  //Read the data from the encoder
+  byte r_update = digitalRead(DT);
+  float oldangright = ang_right;
+
+  //Check which way its going
+  if(r_update != digitalRead(CLK)){
+
+    
+    ang_right += 1; 
+
+    if(ang_right > rot-1){
+      ang_right = 0;
+    }
+    
+    
+  } else{
+   
+    ang_right -= 1; 
+
+    if(ang_right < 0){
+      ang_right = rot - 1;
+    }
+    
+  }
+
+  
+
+}
+      
+void  rotate_l(){
+
+  //Read the data from the encoder
+  byte l_update = digitalRead(DT2);
+  float ang_old = ang_left;
+  //Check which way its going
+  if(l_update != digitalRead(CLK2)){
+    
+    ang_left += 1; 
+
+    if(ang_left > rot-1){
+      ang_left = 0;
+    }
+    
+  } else{
+
+    ang_left -= 1; 
+
+    if(ang_left < 0){
+      ang_left = rot-1;
+    }
+    
+  }
+
+   
+ 
+}
